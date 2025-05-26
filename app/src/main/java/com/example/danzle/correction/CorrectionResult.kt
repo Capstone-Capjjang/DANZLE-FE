e6ba66aaee233787e8823cf57111c1c1af923851
sohee <144779368+sohee6989@users.noreply.github.com>
2025-05-27 08:57:46 +0900

    feat: Complete UI of CorrectionResult.kt

--- app/src/main/java/com/example/danzle/correction/CorrectionResult.kt
+++ app/src/main/java/com/example/danzle/correction/CorrectionResult.kt
@@ -2,14 +2,21 @@ package com.example.danzle.correction
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.danzle.MainActivity
 import com.example.danzle.R
 import com.example.danzle.data.api.DanzleSharedPreferences
+import com.example.danzle.data.api.RetrofitApi
+import com.example.danzle.data.remote.response.auth.CorrectionResultResponse
 import com.example.danzle.databinding.ActivityCorrectionResultBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CorrectionResult : AppCompatActivity() {
     private lateinit var binding: ActivityCorrectionResultBinding
@@ -40,15 +47,11 @@ class CorrectionResult : AppCompatActivity() {
             }
         }
 
-        val sessionId = 729L
+        val sessionId = intent.getLongExtra("sessionId", -1L)
 
-        //retrofitCorrectionResult(sessionId, authHeader)
-
-//        val sessionId = intent.getLongExtra("sessionId", -1L)
-//
-//        if (sessionId != -1L) {
-//            retrofitCorrectionResult(sessionId, authHeader)
-//        }
+        if (sessionId != -1L) {
+            retrofitCorrectionResult(authHeader, sessionId)
+        }
 
         binding.feedback.setOnClickListener {
             val intent = Intent(this, CorrectionDetailFeedback::class.java)
@@ -57,139 +60,159 @@ class CorrectionResult : AppCompatActivity() {
         }
     }
 
-//    private fun retrofitCorrectionResult(sessionId: Long, authHeader: String) {
-//
-//        binding.progressBar.visibility = View.VISIBLE
-//        binding.resultContentGroup.visibility = View.GONE
-//
-//        val retrofit = RetrofitApi.getCorrectionResultInstance()
-//
-//        retrofit.getCorrectionResultService(sessionId, authHeader)
-//            .enqueue(object : Callback<CorrectionResultResponse> {
-//                override fun onResponse(
-//                    call: Call<CorrectionResultResponse>,
-//                    response: Response<CorrectionResultResponse>
-//                ) {
+    private fun retrofitCorrectionResult(authHeader: String, sessionId: Long) {
+
+        //binding.progressBar.visibility = View.VISIBLE
+        //binding.resultContentGroup.visibility = View.GONE
+
+        val retrofit = RetrofitApi.getCorrectionResultInstance()
+
+        retrofit.getCorrectionResultService(authHeader, sessionId)
+            .enqueue(object : Callback<CorrectionResultResponse> {
+                override fun onResponse(
+                    call: Call<CorrectionResultResponse>,
+                    response: Response<CorrectionResultResponse>
+                ) {
 //                    binding.progressBar.visibility = View.GONE
 //                    binding.resultContentGroup.visibility = View.VISIBLE
-//
-//                    if (response.isSuccessful) {
-//                        val result = response.body() ?: return
-//                        Log.d("CorrectionResult", "Response success: $result")
-//
-//                        val excellent = result.excellent
-//                        val good = result.good
-//                        val normal = result.normal
-//                        val bad = result.bad
-//                        val miss = result.miss
-//
-//                        binding.countExcellent.text = excellent.toString()
-//                        binding.countGood.text = good.toString()
-//                        binding.countNormal.text = normal.toString()
-//                        binding.countBad.text = bad.toString()
-//                        binding.countMiss.text = miss.toString()
-//
-//                        val total = excellent + good + normal + bad + miss
-//                        val maxBarWidthDp = 100
-//
-//                        if (total > 0) {
-//                            // 각 bar의 비율 기반 너비 설정
-//                            setBarWidth(binding.barExcellent, excellent, total, maxBarWidthDp)
-//                            setBarWidth(binding.barGood, good, total, maxBarWidthDp)
-//                            setBarWidth(binding.barNormal, normal, total, maxBarWidthDp)
-//                            setBarWidth(binding.barBad, bad, total, maxBarWidthDp)
-//                            setBarWidth(binding.barMiss, miss, total, maxBarWidthDp)
-//                        }
-//
-//                        if (result == null) {
-//                            Log.e("CorrectionResult", "Response body is null")
-//                            return
-//                        }
-//
-//                        val level = result.resultLevel ?: "Unknown"
-//
-//                        when (level) {
-//                            "Perfect" -> {
-//                                binding.badgeResultText.text = "Perfect"
-//                                binding.badgeResultText.setTextColor(
-//                                    ContextCompat.getColor(
-//                                        this@CorrectionResult,
-//                                        R.color.scorePerfectText
-//                                    )
-//                                )
-//                            }
-//
-//                            "Good" -> {
-//                                binding.badgeResultText.text = "Good"
-//                                binding.badgeResultText.setTextColor(
-//                                    ContextCompat.getColor(
-//                                        this@CorrectionResult,
-//                                        R.color.scoreGoodText
-//                                    )
-//                                )
-//                            }
-//
-//                            "Normal" -> {
-//                                binding.badgeResultText.text = "Normal"
-//                                binding.badgeResultText.setTextColor(
-//                                    ContextCompat.getColor(
-//                                        this@CorrectionResult,
-//                                        R.color.scoreNormalText
-//                                    )
-//                                )
-//                            }
-//
-//                            "Bad" -> {
-//                                binding.badgeResultText.text = "Bad"
-//                                binding.badgeResultText.setTextColor(
-//                                    ContextCompat.getColor(
-//                                        this@CorrectionResult,
-//                                        R.color.scoreBadText
-//                                    )
-//                                )
-//                            }
-//
-//                            else -> {
-//                                binding.badgeResultText.text = "Unknown"
-//                                binding.badgeResultText.setTextColor(
-//                                    ContextCompat.getColor(
-//                                        this@CorrectionResult,
-//                                        R.color.grayText
-//                                    )
-//                                )
-//                            }
-//                        }
-//
-//                    } else {
-//                        Log.e("CorrectionResult", "Response failed with code: ${response.code()}")
-//                        Log.e("CorrectionResult", "Error body: ${response.errorBody()?.string()}")
-//                    }
-//
-//                }
-//
-//                override fun onFailure(
-//                    call: Call<CorrectionResultResponse>,
-//                    t: Throwable
-//                ) {
-//                    binding.progressBar.visibility = View.GONE
-//                    Log.e("CorrectionResult", "Network failure: ${t.message}")
-//                }
-//            }
- //           )
+
+                    if (response.isSuccessful) {
+                        val result = response.body() ?: return
+                        Log.d("CorrectionResult", "Response success: $result")
+
+                        val excellent = result.excellent
+                        val good = result.good
+                        val normal = result.normal
+                        val bad = result.bad
+                        val miss = result.miss
+
+                        binding.countExcellent.text = excellent.toString()
+                        binding.countGood.text = good.toString()
+                        binding.countNormal.text = normal.toString()
+                        binding.countBad.text = bad.toString()
+                        binding.countMiss.text = miss.toString()
+
+                        val total = excellent + good + normal + bad + miss
+                        val maxBarWidthDp = 100
+
+                        if (total > 0) {
+                            binding.progressExcellent.max = total
+                            binding.progressExcellent.progress = excellent
+
+                            binding.progressGood.max = total
+                            binding.progressGood.progress = good
+
+                            binding.progressNormal.max = total
+                            binding.progressNormal.progress = normal
+
+                            binding.progressBad.max = total
+                            binding.progressBad.progress = bad
+
+                            binding.progressMiss.max = total
+                            binding.progressMiss.progress = miss
+                        }
+
+                        if (result == null) {
+                            Log.e("CorrectionResult", "Response body is null")
+                            return
+                        }
+
+                        val level = result.resultLevel ?: "Unknown"
+
+                        // ContextCompat.getColor()는 버전 상관없이 색상을 안전하게 적용해준다.
+                        when (level) {
+                            "Excellent" -> {
+                                binding.badgeResultText.text = "Excellent"
+                                binding.badgeResultText.background.setTint(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult,
+                                        R.color.scorePerfectBackground
+                                    )
+                                )
+                                binding.badgeResultText.setTextColor(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult, R.color.scorePerfectText
+                                    )
+                                )
+                            }
+
+                            "Good" -> {
+                                binding.badgeResultText.text = "Good"
+                                binding.badgeResultText.background.setTint(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult,
+                                        R.color.scoreGoodBackground
+                                    )
+                                )
+                                binding.badgeResultText.setTextColor(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult, R.color.scoreGoodText
+                                    )
+                                )
+                            }
+
+                            "Normal" -> {
+                                binding.badgeResultText.text = "Normal"
+                                binding.badgeResultText.background.setTint(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult,
+                                        R.color.scoreNormalBackground
+                                    )
+                                )
+                                binding.badgeResultText.setTextColor(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult, R.color.scoreNormalText
+                                    )
+                                )
+                            }
+
+                            "Bad" -> {
+                                binding.badgeResultText.text = "Bad"
+                                binding.badgeResultText.background.setTint(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult,
+                                        R.color.scoreBadBackground
+                                    )
+                                )
+                                binding.badgeResultText.setTextColor(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult, R.color.scoreBadText
+                                    )
+                                )
+                            }
+
+                            else -> {
+                                binding.badgeResultText.text = "Miss"
+                                binding.badgeResultText.background.setTint(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult,
+                                        R.color.scoreMissBackground
+                                    )
+                                )
+                                binding.badgeResultText.setTextColor(
+                                    ContextCompat.getColor(
+                                        this@CorrectionResult, R.color.grayText
+                                    )
+                                )
+                            }
+                        }
+
+                    } else {
+                        Log.e("CorrectionResult", "Response failed with code: ${response.code()}")
+                        Log.e("CorrectionResult", "Error body: ${response.errorBody()?.string()}")
+                    }
+
+                }
+
+                override fun onFailure(
+                    call: Call<CorrectionResultResponse>,
+                    t: Throwable
+                ) {
+                    // binding.progressBar.visibility = View.GONE
+                    Log.e("CorrectionResult", "Network failure: ${t.message}")
+                }
+            }
+            )
 
     }
-
-//    private fun dpToPx(dp: Int): Int {
-//        val density = resources.displayMetrics.density
-//        return (dp * density).toInt()
-//    }
-//
-//    private fun setBarWidth(view: View, value: Int, total: Int, maxBarWidthDp: Int) {
-//        val ratio = value.toFloat() / total
-//        val newWidthPx = dpToPx((maxBarWidthDp * ratio).toInt())
-//
-//        val layoutParams = view.layoutParams
-//        layoutParams.width = newWidthPx
-//        view.layoutParams = layoutParams
-//    }
-//}
+}
